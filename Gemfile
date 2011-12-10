2531236d03ff80db2a144b1627a4254de87cb69f
Andrew Grieser <agrieser@gmail.com>
2011-12-10 12:44:47 -0500

    Generate rails project

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source :rubygems
+
+gem 'rails', '3.1.3'
+gem 'jquery-rails'
+gem 'haml'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'haml-rails'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odk::Application.config.secret_token = 'b62bb18d8f99f564cb40d059ba8e277755bfd3ecfec200463f1a368ed03e9aeabeabe1c319d0dac297911b2475712ebf6e9d0c5d81310728132a97910ac27d46'
